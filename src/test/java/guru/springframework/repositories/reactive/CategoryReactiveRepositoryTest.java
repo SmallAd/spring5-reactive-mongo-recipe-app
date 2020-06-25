c4b25d47fbe7c7610285ea56d21f6e7fdf51edbc
SmallAd <rbyj-ujdyj11>
2020-06-25 18:55:08 +0300

    Reactive repositories added

---
+++ src/test/java/guru/springframework/repositories/reactive/CategoryReactiveRepositoryTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.Category;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class CategoryReactiveRepositoryTest {
+
+    public static final String FOO = "Foo";
+    @Autowired
+    CategoryReactiveRepository categoryReactiveRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        categoryReactiveRepository.deleteAll().block();
+
+        Category category = new Category();
+        category.setDescription(FOO);
+
+        categoryReactiveRepository.save(category).block();
+    }
+
+    @Test
+    public void findByDescription() {
+        Category fetchedCategory = categoryReactiveRepository.findByDescription(FOO).block();
+        assertNotNull(fetchedCategory.getId());
+    }
+
+    @Test
+    public void testSave() {
+        Long count = categoryReactiveRepository.count().block();
+        assertEquals(Long.valueOf(1L), count);
+    }
+}
